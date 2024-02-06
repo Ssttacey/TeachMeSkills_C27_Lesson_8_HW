3f8635baeed4e7392de77353b2fcfc666c36dbe1
Ssttacey <sloypokg@gmail.com>
2024-02-06 17:41:57 +0300

    commit

--- src/com/techmeskills/lesson8/Runner.java
+++ src/com/techmeskills/lesson8/Runner.java
@@ -4,7 +4,7 @@ import com.techmeskills.lesson8.medStaff.Therapist;
 import com.techmeskills.lesson8.patient.Patient;
 
 
-public class Runner {
+public class  Runner {
     public static void main(String[] args) {
 
         Patient patient1 = new Patient("name", 1);
